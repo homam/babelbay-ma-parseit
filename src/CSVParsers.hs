0eb3691415729ba1623f7bdbddd835f3443f8011
Homam <bluesnowball@gmail.com>
2015-12-13 18:00:42 +0400

    first col parser

--- src/CSVParsers.hs
+++ src/CSVParsers.hs
@@ -3,24 +3,12 @@
 module CSVParsers (
     Language
   , Dic
-  , CSVFlashcard
-  , csvFCDic
-  , csvFCIndex
-  , CSVChapter
-  , csvChapterIndex
-  , csvChapterDic
-  , csvChapterCards
+  , CSVFlashcard (..)
+  , CSVChapter (..)
   , toCSVChapters
   , DicOf
-  , CSVCourseIntro
-  , csvCourseTitle
-  , csvCourseIntroTitle
-  , csvCourseIntroFC1
-  , csvCourseIntroFC2
-  , CSVCourseIntroFlashcard
-  , csvCiFcQuestion
-  , csvCiFcShortAns
-  , csvCiFcLongAns
+  , CSVCourseIntro (..)
+  , CSVCourseIntroFlashcard (..)
   , toCSVCourseIntro
 ) where
 
@@ -31,7 +19,7 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import Data.Either
-
+import Text.Read (readEither)
 
 
 
@@ -41,7 +29,7 @@ type Language = String
 
 type Dic = M.Map Language String
 
-data CSVFlashcard = Flashcard {
+data CSVFlashcard = CSVFlashcard {
     csvFCIndex :: Integer
   , csvFCDic :: Dic
 } deriving Show
@@ -64,7 +52,7 @@ toCSVChapter :: [[String]] -> Integer -> CSVChapter
 toCSVChapter matrix index = CSVChapter index (toDic $ drop 1 $ head matrix) (map toFlashcard $ drop 1 matrix)
 
 toFlashcard :: [String] -> CSVFlashcard
-toFlashcard list = Flashcard (read $ head list) (toDic $ drop 1 list)
+toFlashcard list = CSVFlashcard (read $ head list) (toDic $ drop 1 list)
 
 toDic :: [String] -> M.Map Language String
 toDic list = M.fromList $ ["en","ar","fr","de","ru","es"] `zip` list
@@ -75,6 +63,9 @@ csvToChapters file =
       schapters = drop 1 $ splitWhen ((== "") . head) csv
   in zipWith toCSVChapter schapters [1 ..]
 
+
+csvFirstCol :: String -> Either String (Integer, Integer, String)
+csvFirstCol col = readEither col :: Either String (Integer, Integer, String)
 
 -- exported
 toCSVChapters :: BL.ByteString -> Either String [CSVChapter]
